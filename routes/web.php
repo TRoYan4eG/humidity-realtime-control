3cea47cef3ba1e6e2d005006f2acd4aed8154369
Tolik Tihosov <djtroyan4eg@gmail.com>
2017-05-15 20:02:50 +0300

    Add authorization & native homepage

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Маршруты аутентификации...
+Route::get('auth/login', 'Auth\LoginController@showLoginForm');
+Route::post('auth/login', 'Auth\LoginController@login');
+Route::get('auth/logout', 'Auth\LoginController@logout');
+
+// Маршруты регистрации...
+Route::get('auth/register', 'Auth\RegisterController@showRegistrationForm');
+Route::post('auth/register', 'Auth\RegisterController@register');
